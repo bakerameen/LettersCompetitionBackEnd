0a857178120e3a766d9e7b2a5969c383c00eabce
Al-baqer Ameen Abbas <baqerabbas@aas.com.sa>
2021-01-03 13:49:36 +0300

    update production var

--- server.js
+++ server.js
@@ -51,13 +51,12 @@ const PORT = process.env.PORT || 8080;
     
 
     // production 
-    // if(process.env.NODE_ENV === 'production') {
-      app.use(express.static(path.join(__dirname, 'public')));
+     app.use(express.static(path.join(__dirname, 'public')));
       app.get('*', (req, res) => {
-        // res.sendFile(path.join(__dirname, 'public/index.html'));
+        //res.sendFile(path.join(__dirname, 'public/index.html'));
          res.sendFile(path.join(__dirname, 'public',  'index.html'));
         });
-    // }
+    
 
     
 
