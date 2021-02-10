9821827238d2adc2157a7b60f2978f6d4babddbf
Al-baqer Ameen Abbas <baqerabbas@aas.com.sa>
2021-02-10 12:53:56 +0300

    borad letters

--- route/board.js
+++ route/board.js
@@ -5,7 +5,29 @@ const router = express.Router();
 const Board = require('../models/board');
 const checkAuth = require('../middleware/check-auth');
 
-router.get('', (req, res, next) => {
+router.post('', checkAuth, (req, res, next) => {
+    console.log(req.body);
+     const board = new Board({
+         letter: req.body.letter,
+         color: req.body.color,
+         fontcolor: req.body.fontcolor 
+     });
+    board.save().then(createdBoard => {
+      res.status(201).json({
+          message: 'Letter Created Successfully!',
+          boardId: createdBoard._id          
+      })
+    })
+
+    .catch( error => {
+        res.status(500).json({
+            message: 'Creating a Letter failed!'
+        })
+    })
+
+})
+
+router.get('', checkAuth, (req, res, next) => {
     Board.find().then(documents =>{
         res.status(200).json({
             message: 'Board Fetched successfully!',
@@ -20,8 +42,10 @@ router.get('', (req, res, next) => {
 })
 
 
+
+
 router.post('', (req, res, next) => {
-    
+
 })
 
 module.exports = router;
